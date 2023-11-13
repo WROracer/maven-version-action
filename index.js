85aa4e64fde688754de425d6d6f759ab95f99c42
WRORacer <felix.loerke@T-online.de>
2023-11-13 10:27:01 +0100

    added release branch creating

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 
-function proccesVersion(version){
+function processVersion(version){
     console.log(`Version : ${version}`)
+    let minor = version.split(".")[1];
+    let major = version.split(".")[0];
+    let patch = version.split(".")[2];
+    let snapshot = version.split("-");
+
+    patch++;
+
+
+    let newVersion = major+"."+minor+"."+patch+"-"+snapshot;
+    let newRelease = major+"."+minor+"."+patch;
+
+    //create new Branch "release/version" and commit
+
+    execSync(`git branch release/${newRelease}`)
+    execSync(`git checkout release/${newRelease}`)
+
+    execSync(`mvn versions:set -DnewVersion=${newRelease}`)
+
+    execSync(`git commit -m "[ACTION] Release version ${newRelease}"`)
+
+    execSync(`git push -u origin -m release/${newRelease}`)
+
+    //commit with newVersion in master
+
 }
 
 try {
@@ -18,7 +42,7 @@ try {
     console.log(`The event payload: ${payload}`);*/
     // mvn help:evaluate -Dexpression=project.version -q -DforceStdout
 
-    exec("mvn help:evaluate -Dexpression=project.version -q -DforceStdout", (error, stdout, stderr) => {
+    execSync("mvn help:evaluate -Dexpression=project.version -q -DforceStdout", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -27,8 +51,7 @@ try {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
-        proccesVersion(`${stdout}`)
+        processVersion(`${stdout}`)
     });
 } catch (error) {
     core.setFailed(error.message);
