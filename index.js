dcc3ce6461da645ac097198e30e3062f3bfdda32
WRORacer <felix.loerke@T-online.de>
2023-11-13 09:55:06 +0100

    added first tests

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const { exec } = require("child_process");
+
+function proccesVersion(version){
+    console.log(`Version : ${version}`)
+}
+
 try {
-    // `who-to-greet` input defined in action metadata file
+    /*// `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    console.log(`The event payload: ${payload}`);*/
+    // mvn help:evaluate -Dexpression=project.version -q -DforceStdout
+
+    exec("mvn help:evaluate -Dexpression=project.version -q -DforceStdout", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        proccesVersion(`${stdout}`)
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
